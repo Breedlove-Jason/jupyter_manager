6f23843eeb37f2b80e09a9549f6e9b25e2a80af1
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-06-14 15:05:17 -0700

    first commit

---
+++ jupyter_manager.py
@@ -0,0 +1,10 @@
+import os
+import threading
+import subprocess
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, \
+    QFileDialog, QTextEdit, QErrorMessage, QComboBox
+from PyQt5.QtCore import pyqtSignal, Qt
+from colorama import init
+
+
