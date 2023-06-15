498bb17dd95087abeff8128fadd8b953e69b90ae
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-06-15 11:35:21 -0700

    create ui

--- jupyter_manager.py
+++ jupyter_manager.py
@@ -8,3 +8,76 @@ from PyQt5.QtCore import pyqtSignal, Qt
 from colorama import init
 
 
+class JupyterManager(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        self.process = None
+
+        # Import Fields
+        self.env_label = QLabel('Environment Name')
+        self.env_label.setStyleSheet('font-size: 15px; font-weight: bold;')
+        self.env_input = QLineEdit()
+        self.env_input.setStyleSheet('font-size: 15px; "background-color: #9D73FA; font-weight: bold;')
+        self.env_input.setPlaceholderText('Enter Environment Name')
+
+        self.pkg_label = QLabel('Package Name')
+        self.pkg_label.setStyleSheet('font-size: 15px; font-weight: bold;')
+        self.pkg_input = QLineEdit()
+        self.pkg_input.setStyleSheet('font-size: 15px; "background-color: #9D73FA; font-weight: bold;')
+        self.pkg_input.setPlaceholderText('Enter Package Name (comma-separated)')
+
+        self.port_label = QLabel('Port Number')
+        self.port_label.setStyleSheet('font-size: 15px; font-weight: bold;')
+        self.port_input = QLineEdit()
+        self.port_input.setStyleSheet('font-size: 15px; "background-color: #9D73FA; font-weight: bold;')
+        self.port_input.setPlaceholderText('Enter Port Number')
+
+        # Create and launch button
+        self.create_button = QPushButton('Create and Launch')
+        self.create_button.setStyleSheet('background-color #9D73FA; font-size: 15px; font-weight: bold;')
+        self.create_button.clicked.connect(self.create_env_launch_jupyter)
+
+        # Kill button
+        self.kill_button = QPushButton('Kill Jupyter Server')
+        self.kill_button.setStyleSheet('background-color #9D73FA; font-size: 15px; font-weight: bold;')
+        self.kill_button.clicked.connect(self.kill_jupyter)
+
+        # Text output field
+        self.text_output = QTextEdit()
+        self.text_output.setStyleSheet('background-color #F060C3; font-size: 15px; font-weight: bold;')
+        self.init_ui()
+
+    def init_ui(self):
+        # Add labels and input fields to layout
+        form_layout = QVBoxLayout()
+        form_layout.addWidget(self.env_label)
+        form_layout.addWidget(self.env_input)
+        form_layout.addWidget(self.pkg_label)
+        form_layout.addWidget(self.pkg_input)
+        form_layout.addWidget(self.port_label)
+        form_layout.addWidget(self.port_input)
+
+        # Add buttons to layout
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.create_button)
+        button_layout.addWidget(self.kill_button)
+
+        # Add form and button layouts to main layout
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(form_layout)
+        main_layout.addLayout(button_layout)
+        main_layout.addWidget(self.text_output)
+
+        # Set main layout and window properties
+        self.setLayout(main_layout)
+        self.setWindowTitle('Jupyter Manager')
+        self.setGeometry(300, 300, 500, 500)
+        self.setStyleSheet('background-color: #332EF0')
+        self.show()
+
+    def create_env_launch_jupyter(self):
+        pass
+
+    def kill_jupyter(self):
+        pass
